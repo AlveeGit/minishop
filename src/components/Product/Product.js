8157609ebea53af82bce47c921d99fb9d6480b16
AlveeGit <alveekabir1@gmail.com>
2023-04-05 15:18:15 +0600

    product card updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    
-    const {name, img, seller, price, ratings} = props.product;
+
+    const { name, img, seller, price, ratings } = props.product;
 
     return (
         <div className='product'>
 
             <img src={img} alt="" />
-            <p className="product-name">{name}</p>
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p className="product-price">Price : ${price}</p>
+                <p className="product-seller">Seller : {seller}</p>
+                <p className="product-ratings">Ratings : {ratings}</p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+
 
 
         </div>
